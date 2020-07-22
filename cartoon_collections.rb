0ea80a8536753b94ee793fe4fe0daed91a8c9359
Steven Williams <evilgeniusnyc@me.com>
2020-07-22 09:53:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,11 @@
 def greet_characters(array)
-  array.each |element|
-    element == "" # alternative solution: element.class == String && element.empty?
+  array.each do |character|
+    puts greeting
   end
 end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
